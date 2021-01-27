437110e820f87ed26c27ac8dfb9491ee166a7a36
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-01-27 14:58:36 +0100

    Updated used API

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch")
 exports.help = true
 exports.run = async (client, target, context, msg, self) => {
-  let uptimerequest = await fetch("https://beta.decapi.me/twitch/uptime/blizzor96")
+  let uptimerequest = await fetch("https://decapi.me/twitch/uptime/blizzor96")
   let uptime = await uptimerequest.text()
   uptime = uptime.replace("hours", "Stunden").replace("hour", "Stunde").replace("minutes", "Minuten").replace("minute", "Minute").replace("seconds", "Sekunden").replace("second", "Sekunde")
   if (uptime == "blizzor96 is offline") {
