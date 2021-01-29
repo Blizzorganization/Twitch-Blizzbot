ab11908fdbdd74810965d7a7782b8a70331acfb8
BlackDemonFire <bdf.sop@gmail.com>
2021-01-29 16:38:30 +0100

    changed variable in blacklist command

--- commands/twitch/functions/blacklist.js
+++ commands/twitch/functions/blacklist.js
@@ -1,6 +1,6 @@
 exports.help = false
 exports.run = (client, target, context, msg, self, args) => {
   if (!context.mod) return
-  client.discord.channel.send(`In der Blacklist für ${target} sind die Wörter \`\`\`fix\n${client.blacklist.get("delmsg").join("\n")}\`\`\` enthalten.`, {split:true})
+  client.clients.discord.blchannel.send(`In der Blacklist für ${target} sind die Wörter \`\`\`fix\n${client.blacklist.get("delmsg").join("\n")}\`\`\` enthalten.`, {split:true})
   console.log(`* Sent the blacklist to ${context.username}`)
 }
