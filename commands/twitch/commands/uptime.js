f547ae5536bb7ef791471e4494f0c552c333d460
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-03-02 16:19:35 +0100

    changes to the variables

--- commands/twitch/commands/uptime.js
+++ commands/twitch/commands/uptime.js
@@ -1,12 +1,13 @@
 const fetch = require("node-fetch")
 exports.help = true
 exports.run = async (client, target, context, msg, self) => {
-  let uptimerequest = await fetch("https://decapi.me/twitch/uptime/blizzor96")
+  let uptimerequest = await fetch(`https://decapi.me/twitch/uptime/${target.slice(1)}`)
   let uptime = await uptimerequest.text()
   uptime = uptime.replace("hours", "Stunden").replace("hour", "Stunde").replace("minutes", "Minuten").replace("minute", "Minute").replace("seconds", "Sekunden").replace("second", "Sekunde")
-  if (uptime == "blizzor96 is offline") {
-    client.say(target, "Blizzor ist offline.")
+  if (uptime == `${target.slice(1)} is offline`) {
+    client.say(target, `${target.slice(1)} ist offline.`)
   } else {
-    client.say(target, `Blizzor ist seit ${uptime} live. blizzo2Logo `)
+    client.say(target, `${target.slice(1)} ist seit ${uptime} live. blizzo2Logo `)
   }
 }
+
