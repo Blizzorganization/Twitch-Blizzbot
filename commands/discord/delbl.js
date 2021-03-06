edb8f36f1db9e44809724a94bdaa3e47167cbbd3
BlackDemonFire <bdf.sop@gmail.com>
2021-03-06 19:05:56 +0100

    fixed a bug where you couldn't correctly remove words from blacklist

--- commands/discord/delbl.js
+++ commands/discord/delbl.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     if (!args||args.lenth==0) return message.channel.send("Du musst angeben, was du von der Blacklist entfernen willst!")
-    let blremove = args.join().toLowerCase()
+    let blremove = args.join(" ").toLowerCase()
     if (!client.clients.twitch.blacklist.includes("delmsg", blremove)) return message.channel.send(`"${blremove}" wird nicht gelöscht, kann also auch nicht aus der Blacklist entfernt werden.`)
     client.clients.twitch.blacklist.remove("delmsg", blremove)
     message.channel.send(`"${blremove}" wurde von der Blacklist entfernt`)
--- commands/twitch/functions/delbl.js
+++ commands/twitch/functions/delbl.js
@@ -2,7 +2,7 @@ exports.help = false
 exports.perm = true
 exports.run = (client, target, context, msg, self, args) => {
   if (!args||args.lenth==0) return client.say(target, "Du musst angeben, was du von der Blacklist entfernen willst!")
-  let blremove = args.join().toLowerCase()
+  let blremove = args.join(" ").toLowerCase()
   if (!client.blacklist.includes("delmsg", blremove)) return client.say(target, `"${blremove}" wird nicht gelöscht, kann also auch nicht aus der Blacklist entfernt werden.`)
   client.blacklist.remove("delmsg", blremove)
   client.say(target, `"${blremove}" wurde von der Blacklist entfernt`)
