ce983ef04ee0b10ca327bc8b627339620545df43
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-03-10 14:19:45 +0100

    update commands

--- commands/twitch/commands/followage.js
+++ commands/twitch/commands/followage.js
@@ -3,7 +3,7 @@ exports.help = true
 exports.run = async (client, target, context, msg, self) => {
     let resp = await fetch(`https://2g.be/twitch/following.php?user=${context["display-name"]}&channel=${target.slice(1)}&format=mwdhms`)
     let followage = await resp.text()
-    followage = followage.replace("years", "Jahre").replace("year", "Jahr").replace("months", "Monate").replace("month", "Monat").replace("weeks", "Wochen").replace("week", "Woche").replace("days", "Tage").replace("day", "Tag").replace("hours", "Stunden").replace("hour", "Stunde").replace("minutes", "Minuten").replace("minute", "Minute").replace("seconds", "Sekunden").replace("second", "Sekunde")
+    followage = followage.replace("years", "Jahre").replace("year", "Jahr").replace("months", "Monaten").replace("month", "Monat").replace("weeks", "Wochen").replace("week", "Woche").replace("days", "Tage").replace("day", "Tag").replace("hours", "Stunden").replace("hour", "Stunde").replace("minutes", "Minuten").replace("minute", "Minute").replace("seconds", "Sekunden").replace("second", "Sekunde")
     followage = followage.replace("has been following", "folgt").replace("for", "seit")
     client.say(target, followage)
 }
