5a6fc88ca91458b3dfdc36abb8ab44a3c13be47c
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-03-14 21:57:24 +0100

    Update subscription.js

--- events/twitch/interaction/subscription.js
+++ events/twitch/interaction/subscription.js
@@ -2,5 +2,5 @@ module.exports = (client, channel, username, method, message, userstate) => {
 
 //message for Action
     console.log(` ${username} subscription`)
-    client.say(channel,`Haaaaaalllooooo und Willkommen ${username} bei den subscrizzors `)
+    client.say(channel,`Haaaaaalllooooo und Willkommen ${username} bei den Subscrizzors `)
 }
