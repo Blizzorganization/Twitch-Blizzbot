2ae9eeb7ad93cae370a6538e288ba11a5817bb96
BlackDemonFire <bdf.sop@gmail.com>
2021-04-22 08:53:29 +0200

    swapped watchtime and mwatchtime commands to work correctly

--- commands/discord/watchtimelb.js
+++ commands/discord/watchtimelb.js
@@ -12,10 +12,10 @@ exports.run = (client, message, args) => {
     if (!args[0]) return message.channel.send("Du musst einen Kananl angeben - folgende sind zur Auswahl:\n`"+client.clients.twitch.getChannels().join("`, `")+ "`")
     const channel = args[0]
     const embed = new MessageEmbed()
-        .setTitle("Monthly Watchtime")
+        .setTitle("Watchtime")
         .setColor(0xdfb82d)
         .setDescription(channel)
-    const watchtime = client.clients.twitch.db.mwatchtimeList(channel, 10)
+    const watchtime = client.clients.twitch.db.watchtimeList(channel, 10)
     for (const user in watchtime) {
         embed.addField(watchtime[user].user, calcWatchtime(watchtime[user].watchtime), false)
     }
