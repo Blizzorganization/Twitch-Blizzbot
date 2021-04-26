d6f65de6a7bb673e22efd01312bd6cf0497b50bf
BlackDemonFire <bdf.sop@gmail.com>
2021-04-26 13:15:15 +0200

    added ! in front of hard coded commands in help command

--- commands/twitch/commands/help.js
+++ commands/twitch/commands/help.js
@@ -10,7 +10,7 @@ exports.alias = ["befehl", "command", "commands", "cmd"]
  * @param {boolean} self
  */
 exports.run = (client, target, context, msg, self) => {
-    let appHelp = client.commands.filter((cmd) => cmd.help).keyArray().join(", ")
+    let appHelp = "!" + client.commands.filter((cmd) => cmd.help).keyArray().join(", !")
     var ccmds = client.db.allCcmds()
     if (ccmds.length > 0) appHelp += `, ${ccmds.join(", ")}`
     client.say(target, `Der Bot kann folgende Commands: ${appHelp}`)
