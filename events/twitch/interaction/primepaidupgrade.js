f47a5d57317df24765382294a81afb8e42050281
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-04-27 09:41:28 +0200

    added primepaidupgrade event

---
+++ events/twitch/interaction/primepaidupgrade.js
@@ -0,0 +1,13 @@
+/**
+ * @listens primepaidupgrade
+ * @param {TwitchClient} client
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} sender
+ * @param {SubUserstate} userstate
+ */
+ exports.event = (client, channel, username, sender, userstate) => {
+    //message for Action
+    console.log(`${username} extended subcription`)
+    client.say(channel, `/me ${username} hat seinen geschenkten Sub verlängert!`)
+ }
