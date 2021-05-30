27c9274b8183ef54c81cdd6960f5f597c60f46ab
speed-r <speedracer2514@gmail.com>
2021-05-30 17:28:57 +0200

    montly output watchtime user

---
+++ commands/discord/mtime.js
@@ -0,0 +1,35 @@
+const { calcWatchtime } = require("../../modules/functions")
+const { MessageEmbed } = require("discord.js")
+
+/**
+ * @name uwtime
+ * @module DiscordCommands
+ * @param {DiscordClient} client
+ * @param {Message} message
+ * @param {string[]} args
+ */
+exports.adminOnly = true
+exports.run = (client, message, args) => {
+    if (!args || args.length == 0) {
+        message.channel.send("Du musst einen Nutzer angeben.")
+        return
+    }
+    let user = "";
+    let channel = client.config.watchtimechannel;
+    while (args.length > 0 && user == "") {
+        if (user == "") {
+            user = args.shift().toLowerCase();
+        }
+    }
+    if (user == "") return message.channel.send("Du musst angeben, für welchen Account du die Watchtime abfragen möchtest.")
+    var mwatchtime = client.clients.twitch.db.getMWatchtime(channel, user)
+    if (!mwatchtime) return message.channel.send("Diesen Nutzer kenne ich nicht.")
+
+    var embed = new MessageEmbed()
+        .setColor(0xdfb82d).setThumbnail(url = "https://blizzor.de/Twitchbot/blizzbot.png")
+        .setTitle("Watchtime")
+        .addField("Nutzername", user)
+        .addField("Montly Watchtime", calcWatchtime(mwatchtime))
+
+    message.channel.send(embed)
+}
