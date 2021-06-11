978670423698f7273fb3e8d0bd5ba1d701b8e69a
speed-r <speedracer2514@gmail.com>
2021-06-12 00:32:14 +0200

    user help command Discord

---
+++ commands/discord/help.js
@@ -0,0 +1,10 @@
+const { Message } = require("discord.js")
+const fs = require("fs")
+const os = require("os")
+const util = require("util")
+const { DiscordClient } = require("../../modules/discordclient")
+
+exports.adminOnly = false
+exports.run = (client, message, args) => {
+    message.channel.send("**__Der Bot kann folgende Befehle__**\n```!top10 Zeigt die Aktuelle Top10 der watchtime an.\n!watchtime (Twitchname) Zeigt die Watchtime von diesem Nutzer an.```")
+}
