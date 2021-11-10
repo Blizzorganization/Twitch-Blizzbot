f8fd8a25f6754a88113c1cbf9405bf661fcd887d
Speed-r <77883172+Speed-r@users.noreply.github.com>
2021-11-10 16:32:37 +0100

    Update help.js

--- commands/discord/commands/help.js
+++ commands/discord/commands/help.js
@@ -16,7 +16,7 @@ exports.run = (client, message) => {
         .addField("!top10", "Gibt die aktuellen Top10 der Watchtime liste wieder")
         .addField("!watchtime [Twitch-Name]", "Gibt die aktuelle watchtime des angegebenen Nutzers wieder")
         .addField("!link [Twitch-Name]", "Um deinen Twitchaccount mit Discord zu verbinden so das du nur noch !watchtime eingeben muss")
-        .addField("!unlink [Twitch-Name]", "Entfernt die Verbinfung zu deinem Twitch Account");
+        .addField("!unlink [Twitch-Name]", "Entfernt die Verbindung zu deinem Twitch Account");
 
     message.channel.send({ embeds: [embed] });
 };
