48bc2d91799c3d8c7e18b142793d7f2dc10be55a
BlackDemonFire <42120952+BlackDemonFire@users.noreply.github.com>
2021-12-24 12:10:45 +0100

    change discord watchtime.js - Percentage added how long you watched the streamer of the streamed time

--- commands/discord/commands/watchtime.js
+++ commands/discord/commands/watchtime.js
@@ -1,6 +1,7 @@
 const { calcWatchtime } = require("../../../modules/functions");
 const { MessageEmbed } = require("discord.js");
 
+exports.alias = ["wt"];
 exports.adminOnly = false;
 /**
  * @name uwtime
@@ -28,13 +29,15 @@ exports.run = async (client, message, args) => {
     }
     if (user == "") return message.channel.send("Du musst angeben, für welchen Account du die Watchtime abfragen möchtest.");
     const watchtime = await client.clients.db.getWatchtime(channel, user, "alltime");
+    const maxWatchtime = await client.clients.db.getWatchtime(channel, client.clients.twitch.getUsername(), "alltime");
     if (!watchtime) return message.channel.send("Diesen Nutzer kenne ich nicht.");
     const embed = new MessageEmbed()
         .setColor(0xedbc5d)
         .setThumbnail(client.user.avatarURL({ format: "png" }))
         .setTitle("Watchtime")
         .addField("Nutzername", user)
-        .addField("Watchtime", calcWatchtime(watchtime));
+        .addField("Watchtime", `${calcWatchtime(watchtime)}`)
+        .addField("Von der gestreamten Zeit", `${Math.round(1000 * watchtime / maxWatchtime) / 10}%`)
 
     message.channel.send({ embeds: [embed] });
 };
--- commands/discord/slash/watchtime.js
+++ commands/discord/slash/watchtime.js
@@ -29,12 +29,14 @@ module.exports = {
             return interaction.reply("Du musst dich zuerst registrieren - /link");
         }
         const watchtime = await client.clients.db.getWatchtime(channel, twuser, "alltime");
+        const maxWatchtime = await client.clients.db.getWatchtime(channel, client.clients.twitch.getUsername(), "alltime");
         const embed = new MessageEmbed()
             .setColor(0xedbc5d)
             .setThumbnail(client.user.avatarURL({ format: "png" }))
             .setTitle("Watchtime")
             .addField("Nutzername", twuser)
-            .addField("Watchtime", calcWatchtime(watchtime));
+            .addField("Watchtime", `${calcWatchtime(watchtime)}`)
+            .addField("Von der gestreamten Zeit", `${Math.round(1000 * watchtime / maxWatchtime) / 10}%`)
         await interaction.reply({ embeds: [embed] });
     },
 };
