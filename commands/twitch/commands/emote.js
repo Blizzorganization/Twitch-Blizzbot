f804a327f2782e39faefb2c2c53c5e38dcefc56f
BlackDemonFire <42120952+BlackDemonFire@users.noreply.github.com>
2022-03-25 20:55:00 +0100

    change emote.js

--- commands/twitch/commands/emote.js
+++ commands/twitch/commands/emote.js
@@ -5,6 +5,7 @@ export const help = true;
 export const perm = permissions.mod;
 export const alias = ["emotes"];
 /**
+ * @author Speed-r
  * @param {import("twitch-blizzbot/twitchclient").TwitchClient} client
  * @param {string} target
  * @param {import("tmi.js").ChatUserstate} context
