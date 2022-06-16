a19a4d27f631d545e57be8b2c9d7a2390e3979fe
BlackDemonFire <lucy@blackdemon.codes>
2022-06-16 17:57:14 +0200

    update files and packages

--- events/twitch/connected.js
+++ events/twitch/connected.js
@@ -20,13 +20,27 @@ export async function event(client, addr, port) {
     client.clients.db.loadBlacklist();
     client.watchtime = setInterval(async () => {
         for (const channel of client.channels) {
-            const uptime = await (await fetch(`https://decapi.me/twitch/uptime/${channel.slice(1)}`)).text();
+            const uptimeRequest = await fetch(`https://decapi.me/twitch/uptime/${channel.slice(1)}`).catch((err) => {
+                logger.error(`Received an Error trying to fetch the uptime API: ${err}`);
+            });
+            if (!uptimeRequest) return;
+            const uptime = await uptimeRequest.text().catch((err) => {
+                logger.error(`Got an error trying to parse a string out of the uptime request: ${err}`);
+            });
+            if (!uptime) return;
             if (`#${uptime}` !== `${channel} is offline`) {
+                const chattersRequest = await fetch(
+                    `http://tmi.twitch.tv/group/user/${channel.slice(1)}/chatters`,
+                ).catch((err) => {
+                    logger.error(`Received an Error trying to fetch the current list of viewers: ${err}`);
+                });
+                if (!chattersRequest) return;
                 /** @type {{chatters: {[key: string]: string[]}}} */
                 // @ts-ignore
-                const jsonResponse = await (
-                    await fetch(`http://tmi.twitch.tv/group/user/${channel.slice(1)}/chatters`)
-                ).json();
+                const jsonResponse = await chattersRequest.json().catch((err) => {
+                    logger.error(`Received an Error trying to parse JSON out of the chatters Request: ${err}`);
+                });
+                if (!jsonResponse) return;
                 const active = jsonResponse.chatters;
                 const viewers = flatMap(active);
                 await client.clients.db.watchtime(channel, flatMap(viewers));
