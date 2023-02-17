e5837794214011c792d047a0be083621d80c9c2b
Speed-r <77883172+Speed-r@users.noreply.github.com>
2023-02-17 10:37:22 +0100

    change structure raid.js

--- commands/twitch/commands/raid.js
+++ commands/twitch/commands/raid.js
@@ -15,13 +15,13 @@ export function run(client, target) {
     const minutes = client.config.Raidminutes;
     setTimeout(ende, 60000 * minutes);
 
+    client.say(target, `/followersoff `);
     client.say(target, `/me Der Follower Modus wurde für die nächsten ${minutes} Minuten deaktiviert.`);
-    client.say(target, "/followersoff");
     /**
      * starts follower only mode after timeout
      */
     function ende() {
-        client.say(target, "/followers 5");
-        client.say(target, "/me Der Follower Modus wurde Aktiviert.");
+        client.say(target, `/followers 5 `);
+        client.say(target, `/me Der Follower Modus wurde Aktiviert.`);
     }
 }
