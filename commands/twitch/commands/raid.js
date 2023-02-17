ac965b007a0ecc6d82a67c269c799ed71986a373
Speed-r <77883172+Speed-r@users.noreply.github.com>
2023-02-17 10:35:50 +0100

    change structure raid.js

--- commands/twitch/commands/raid.js
+++ commands/twitch/commands/raid.js
@@ -1,24 +1,27 @@
-const { permissions } = require("../../../modules/constants");
+import { permissions } from "twitch-blizzbot/constants";
 
-exports.help = false;
-exports.perm = permissions.mod;
+export const help = false;
+export const perm = permissions.vip;
+/** @type {string[]} */
+export const alias = [];
 /**
- * @name raid
+ * @author Speed-r
+ * @name Raidminutes
  * @namespace TwitchCommands
- * @param {import("../../../modules/twitchclient").TwitchClient} client
+ * @param {import("twitch-blizzbot/twitchclient").TwitchClient} client
  * @param {string} target
  */
-exports.run = (client, target) => {
+export function run(client, target) {
     const minutes = client.config.Raidminutes;
     setTimeout(ende, 60000 * minutes);
 
+    client.say(target, `/followersoff `);
     client.say(target, `/me Der Follower Modus wurde für die nächsten ${minutes} Minuten deaktiviert.`);
-    client.say(target, "/followersoff");
     /**
      * starts follower only mode after timeout
      */
     function ende() {
-        client.say(target, "/followers 5");
-        client.say(target, "/me Der Follower Modus wurde Aktiviert.");
+        client.say(target, `/followers 5 `);
+        client.say(target, `/me Der Follower Modus wurde Aktiviert.`);
     }
-};
+}
